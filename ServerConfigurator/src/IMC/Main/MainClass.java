dffa9b0273421842562cb2bb62069b27711fb1a9
ImprovedMC Developer Network <info@improvedmc.org>
2013-07-07 00:31:01 +0200

    Add OnEnable

--- ServerConfigurator/src/IMC/Main/MainClass.java
+++ ServerConfigurator/src/IMC/Main/MainClass.java
@@ -1,5 +1,7 @@
 package IMC.Main;
 
+import org.bukkit.Bukkit;
+
 import IMC.API.BJavaPlugin;
 
 public class MainClass extends BJavaPlugin{
@@ -7,5 +9,9 @@ public class MainClass extends BJavaPlugin{
 	public String defaultPermission;
 	public String consolenPrefix;
 	public String messagePrefix;
+	
+	public void onEnable(){
+		Bukkit.getServer();
+	}
 
 }
